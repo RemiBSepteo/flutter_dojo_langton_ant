346f7d18fc017293560e245f14d1d418efd44636
Rémi Belzanti <remi.belzanti@genapi.fr>
2025-10-31 08:41:22 +0100

    deplacement des fichiers

--- front/langton_flutter/lib/data/dto/protocols/cell_protocol.dart
+++ front/langton_flutter/lib/data/dto/protocols/cell_protocol.dart
@@ -1,5 +1,5 @@
 import 'package:langton_ant/data/dto/cell_dto.dart';
-import 'package:langton_ant/data/protocols/position_protocol.dart';
+import 'package:langton_ant/data/dto/protocols/position_protocol.dart';
 import 'package:langton_ant/domain/entities/cell.dart';
 
 extension CellProtocoll on CellDto {
--- front/langton_flutter/lib/data/dto/protocols/fourmi_protocol.dart
+++ front/langton_flutter/lib/data/dto/protocols/fourmi_protocol.dart
@@ -1,15 +1,16 @@
 import 'package:langton_ant/data/dto/ant_dto.dart';
-import 'package:langton_ant/data/protocols/position_protocol.dart';
+import 'package:langton_ant/data/dto/protocols/position_protocol.dart';
 import 'package:langton_ant/domain/entities/fourmi.dart';
 
 extension FourmiProtocol on AntDto {
   Fourmi toEntity() {
-    final val = switch (orientation) {
+    final intOrientation = switch (orientation) {
       "north" => 0,
       "east" => 1,
       "south" => 2,
       _ => 3,
     };
-    return Fourmi(orientation: val, position: position.toEntity());
+
+    return Fourmi(orientation: intOrientation, position: position.toEntity());
   }
 }
